aea4b291edb15b995da15bd6baeba68249010890
fakhri <fakhri.warnet@live.com>
2021-02-03 14:00:48 +0700

    desa

---
+++ themes/cosmo/commons/footer.php
@@ -0,0 +1,8 @@
+<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<?php if($transparansi) $this->load->view($folder_themes .'/partials/apbdesa', $transparansi) ?>
+
+<!-- MOHON TIDAK MEMODIFIKASI TAUTAN PENGEMBANG DI BAWAH INI SEBAGAI BENTUK PENGHARGAAN HAK CIPTA. -->
+<footer class="footer">
+	
+</footer>
---
+++ themes/natra/partials/footer_bottom.php
@@ -0,0 +1,27 @@
+  <div class="footer_bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="footer_bottom_left">
+              <?php if (file_exists('mitra')): ?>
+              Hosting didukung <a href="https://my.idcloudhost.com/aff.php?aff=3172" rel="noopener noreferrer" target="_blank">
+                  <img src="<?= base_url('/assets/images/Logo-IDcloudhost.png')?>" height='15px' alt="Logo-IDCloudHost" title="Logo-IDCloudHost"></a>
+              <?php endif; ?>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="footer_bottom_right">
+		   &copy; 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<script type="text/javascript">
+function printDiv(divName) {
+	var printContents = document.getElementById(divName).innerHTML;
+	var originalContents = document.body.innerHTML;
+	document.body.innerHTML = printContents;
+	window.print();
+	document.body.innerHTML = originalContents;
+} </script>
